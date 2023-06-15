75c932d1c95486c03e9d529bdef8e4fadfc2275e
KKirimi <keithkirimi.kk@gmail.com>
2023-06-15 09:19:03 -0700

    Ala

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 
 /*
- * main-prints (Programming is like building a multilingual puzzle)
- * Return: Always 0
+ * main-prints "Programming is like building a multilingual puzzle."
+ * Return: Always 0.
  *
  */
 
 int main(void)
 
 {
-	puts("\n", "Programming is like building a multilingual puzzle");
+	puts("\ Programming is like building a multilingual puzzle");
+
 	return (0);
 }
