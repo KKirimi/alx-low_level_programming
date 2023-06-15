56ca276925cda1acd74a6e019d62c66fa32ff27e
KKirimi <keithkirimi.kk@gmail.com>
2023-06-15 07:28:19 -0700

    Don

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 
 /*
- * main- This is the function of the program. 
+ * main-This is the function of the program. 
  * Return: Always 0.
  */
 
 int main(void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
