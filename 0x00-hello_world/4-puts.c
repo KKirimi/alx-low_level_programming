8a331f27e57d35770a38088dce1f235a6ee940a2
KKirimi <keithkirimi.kk@gmail.com>
2023-06-15 09:10:58 -0700

    Ehm

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,13 @@
 
 /*
  * main-This is the function of the program
- * Return:0
+ * Return: Always 0
  *
  */
 
-int maint(void)
+int main(void)
 
 {
-	puts("Programming is like building a multilingual puzzle\n")
-	return=0
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
