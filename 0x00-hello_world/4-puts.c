b6f3905ab077611ae4dfe85810a9dca860ea3bc8
KKirimi <keithkirimi.kk@gmail.com>
2023-06-15 12:02:28 -0700

    Twende

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 
-/*
- * main-puts "Programming is like
- * building a multilingual puzzle."
- * Return: Always 0.
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
  *
  */
 
 int main(void)
 
 {
-	puts("\Programming is like building a multilingual puzzle");
-
-	return (0);
+	puts("Programming is like building a multilingual puzzle\n");
+	return(0);
 }
