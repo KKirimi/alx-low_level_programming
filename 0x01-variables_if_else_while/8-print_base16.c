fbf892e73fc0f1f28ef5d483afcd968bd98ec57d
KKirimi <keithkirimi.kk@gmail.com>
2023-06-16 11:43:15 -0700

    K

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar(c);
+	
+	return (0);
+}
