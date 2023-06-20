c0776c5cc5caeb82cd241c955b4699ec936b47b6
KKirimi <keithkirimi.kk@gmail.com>
2023-06-20 15:04:49 -0700

    Share

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,5 +5,7 @@ void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
+int _islower(int c);
 int _isalpha(int c);
+int _putchar(int c);
 #endif
