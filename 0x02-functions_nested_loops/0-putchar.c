6eace35acbf90cf32e567e03754048cd0c7baba7
KKirimi <keithkirimi.kk@gmail.com>
2023-06-20 11:15:45 -0700

    K

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * Main- Entry point
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+        putchar('_');
+        putchar('p');
+        putchar('u');
+        putchar('t');
+        putchar('c');
+        putchar('h');
+        putchar('a');
+        putchar('r');
+        putchar('\n');
+        return (0);
+}
