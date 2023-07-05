c1438732e53618d49e944a2afedcd7c2eb61fea6
KKirimi <keithkirimi.kk@gmail.com>
2023-07-05 09:33:38 -0700

    C-Recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "string.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: a string
+ *
+ * Return: a string in reverse
+ */
+
+void _print_rev_recursion
+{
+	char *s[5];
+
+	printf("\n");
+	gets(*s);
+
+	strrev(s);
+
+	printf("%s \n", *s);
+
+	return 0;
+}
