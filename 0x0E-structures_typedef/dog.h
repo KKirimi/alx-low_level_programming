bb5cb3015a3a62baf9698c1178baea79463078d6
KKirimi <keithkirimi.kk@gmail.com>
2023-07-17 13:12:38 -0700

    Structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
+#ifndef DOG_H
 #define DOG_H
-
 
 /**
  * struct dog - a dog's basic info
@@ -16,12 +16,10 @@ struct dog
 	char *owner;
 };
 
-
 /**
  * dog_t - typedef for struct dog
  */
 typedef struct dog dog_t;
-
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
@@ -29,6 +27,5 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
-
 
 #endif
