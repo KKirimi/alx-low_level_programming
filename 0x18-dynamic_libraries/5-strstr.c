ccb1c213c86a360bebe3b9a6ed1e1808eda63b6a
KKirimi <keithkirimi.kk@gmail.com>
2023-12-13 05:30:32 -0800

    0x18. C - Dynamic libraries

---
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strstr - locating a substring
+ * @haystack: the string
+ * @needle: the substring
+ * Return: returns a pointer
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int s = 0;
+
+	while (needle[s] != '\0')
+		s++;
+
+	while (*haystack)
+	{
+		for (i = 0; needle[i]; i++)
+		{
+			if (haystack[i] != needle[i])
+				break;
+		}
+		if (i != s)
+			haystack++;
+		else
+			return (haystack);
+	}
+	return (NULL);
+}
